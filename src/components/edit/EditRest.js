14e5a9c5bcea47f4da8de7383378f69730de425b
Gulshan Khandale <105353066+Gulshankhandale@users.noreply.github.com>
2023-01-13 17:26:23 +0530

    Update changes

--- src/components/edit/EditRest.js
+++ src/components/edit/EditRest.js
@@ -1,20 +1,49 @@
-// import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState, useEffect } from "react";
 import React from "react";
 // import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const EditRest = (props) => {
+    
+    const [name, setName] = useState('');
+    const [address, setAddress] = useState('');
+    const [country_code, setCountryCode] = useState('');
+    const [phone, setPhone] = useState('');
+    const [open_time, setOpenTime] = useState('');
+    const [close_time, setCloseTime] = useState('');
+    const [status, setStatus] = useState('');
+    
+    const {id} = useParams();
+    
+    function getRest(rest_id){
+        
+        axios.get(`http://localhost:3000/restaurent/${rest_id}`)
+        .then((res)=>(
+            console.log(res.data)
+        )).catch((err)=>{
+            console.log("Something went wrong")
+        })
+    }
 
 // =========================================== UPDATE RESTAURANT =============================================================
 
-// function updateRest(id){
-//   axios.put(`http://localhost:3000/updaterestaurant/${id}`).then((res)=>{
-//     console.log(res.data)
+ function updateRest(update_id){
+   axios.put(`http://localhost:3000/updaterestaurant/${update_id}`,{
+       name,
+       address,
+       country_code,
+       phone,
+       open_time,
+       close_time,
+       status
+   }).
+    then((res)=>{
+     console.log(res.data)
     
-//   }).catch((err)=>{
-//     console.log("*Restaurants* UNABLE TO UPDATE RESTAURANT!!")
-//   })
-// }
+   }).catch((err)=>{
+     console.log("*Restaurants* UNABLE TO UPDATE RESTAURANT!!")
+   })
+ }
 
   return (
     <div className="container">
@@ -26,33 +55,33 @@ const EditRest = (props) => {
       <form>
   <div className="mb-3">
     <label for="" className="form-label">NAME</label>
-    <input type="text" className="form-control" id="name"/>
+    <input type="text" className="form-control" value={name || ""} id="name" onChange={(e)=>{setName(e.target.value)}}/>
   </div>
   <div className="mb-3">
     <label for="" className="form-label">ADDRESS</label>
-    <input type="text" className="form-control" id="address"/>
+    <input type="text" className="form-control" value={name || ""} id="address" onChange={(e)=>{setAddress(e.target.value)}}/>
   </div>
   <div className="mb-3">
     <label for="" className="form-label">COUNTRY CODE</label>
-    <input type="text" className="form-control" id="ccode"/>
+    <input type="text" className="form-control" value={address || ""} id="ccode" onChange={(e)=>{setCountryCode(e.target.value)}}/>
   </div>
   <div className="mb-3">
     <label for="" className="form-label">PHONE</label>
-    <input type="text" className="form-control" id="phone"/>
+    <input type="text" className="form-control" value={phone || ""} id="phone" onChange={(e)=>{setPhone(e.target.value)}}/>
   </div>
   <div className="mb-3">
     <label for="" className="form-label">OPEN TIME</label>
-    <input type="text" className="form-control" id="ot"/>
+    <input type="text" className="form-control" value={open_time || ""} id="ot" onChange={(e)=>{setOpenTime(e.target.value)}}/>
   </div>
   <div className="mb-3">
     <label for="" className="form-label">CLOSE TIME</label>
-    <input type="text" className="form-control" id="ct"/>
+    <input type="text" className="form-control" value={close_time || ""} id="ct" onChange={(e)=>{setCloseTime(e.target.value)}}/>
   </div>
   <div className="mb-3">
     <label for="" className="form-label">STATUS</label>
-    <input type="text" className="form-control" id="status"/>
+    <input type="text" className="form-control" value={status || ""} id="status" onChange={(e)=>{setStatus(e.target.value)}}/>
   </div>
-  <button /*onClick={updateRest()}*/ type="submit" className="btn btn-primary">UPDATE</button>
+  <button onClick={()=>{updateRest(id)}} className="btn btn-primary">UPDATE</button>
 </form>
     </div>
   )
